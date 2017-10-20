936e948fd029932da333aa71b659d04293c21179
Jeswin Kumar <jeswinpk@agilehead.com>
2017-10-20 10:03:49 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import * as arrowFunctions from "./arrow-functions";
+import * as bindings from "./bindings";
+import * as expressions from "./expressions";
+import * as results from "./results";
+
+export { arrowFunctions, bindings, expressions, results };
+
+export { default as composite } from "./composite";
+export { default as source } from "./source";
+export { default as clean } from "./clean";
---
+++ src/source.js
@@ -0,0 +1,20 @@
+import { parse, any, builtins as $ } from "chimpanzee";
+
+/*
+  Defers eval of schema until we need it.
+  This is necessary to avoid infinite recursion;
+  schemas might contain schemas which contain themselves.
+*/
+export default function(_schemas, params) {
+  return (state, analysisState) =>
+    $.func(
+      (obj, key, parents, parentKeys) => context => {
+        const schemas = typeof _schemas === "function" ? _schemas() : _schemas;
+        const anySchema = any(
+          schemas.map(schema => schema(state, analysisState))
+        );
+        return parse(anySchema)(obj, key, parents, parentKeys)(context);
+      },
+      params || { selector: "path" },
+    );
+}
