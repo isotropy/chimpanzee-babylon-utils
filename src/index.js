93bdc6d9137553ff6abd678f02ab4369fa194daa
Jeswin Kumar <jeswinpk@agilehead.com>
2017-10-28 16:01:31 +0530

    Remove expr

--- src/index.js
+++ src/index.js
@@ -5,6 +5,5 @@ export { bindings };
 export { default as composite } from "./composite";
 export { default as source } from "./source";
 export { default as clean } from "./clean";
-export { default as expr } from "./expr";
 
 export { permute, permuteWith, permuteProps } from "./permute";
