33ff57ce1661d28a115922b22190207b95a021ae
Jeswin Kumar <jeswinpk@agilehead.com>
2017-10-29 16:19:28 +0530

    Move permute to chimpanzee

--- src/index.js
+++ src/index.js
@@ -5,5 +5,3 @@ export { bindings };
 export { default as composite } from "./composite";
 export { default as source } from "./source";
 export { default as clean } from "./clean";
-
-export { permute, permuteObject, permuteProps, permuteArray, permuteIndexes } from "./permute";
